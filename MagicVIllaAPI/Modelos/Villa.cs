530673709ed1506eb313e4bd6c9f8f060bc86971
Bae <79883620+Lautaro-Cejas@users.noreply.github.com>
2023-05-27 16:57:54 -0300

    Limpiando y corrigiendo lineas - parte 4

--- MagicVIllaAPI/Modelos/Villa.cs
+++ MagicVIllaAPI/Modelos/Villa.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MagicVIllaAPI.Modelos
+namespace MagicVillaAPI.Modelos
 {
     public class Villa
     {
